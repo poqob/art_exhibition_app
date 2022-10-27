c2e3f5584a671ca55ed52fcb51bc5286dfd51048
Mustafa <mmermmus123@gmail.com>
2022-10-28 00:13:55 +0300

    LoginPage base developed.

--- lib/pages/auth/LogInPage.dart
+++ lib/pages/auth/LogInPage.dart
@@ -1,3 +1,4 @@
+import 'package:art_exhibition_app/pages/homePage/homeScreen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:art_exhibition_app/utils/Todo.dart';
@@ -16,6 +17,7 @@ Work types:
   2-Will us provide art works from db??? how it is possible ??
 ''')
 class _LogInPageState extends State<LogInPage> {
+  bool _toogle = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,15 +46,22 @@ class _LogInPageState extends State<LogInPage> {
               child: Align(
                 alignment: Alignment.topCenter,
                 heightFactor: 1,
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                        color: const Color.fromARGB(255, 101, 67, 49),
-                        width: 3,
-                        style: BorderStyle.solid),
+                widthFactor: 1,
+                child: AnimatedOpacity(
+                  duration: const Duration(milliseconds: 1200),
+                  opacity: !_toogle ? 1.0 : 0.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: const Color.fromARGB(255, 101, 67, 49),
+                          width: 3,
+                          style: BorderStyle.solid),
+                    ),
+                    child: Image.network(
+                      func(_toogle),
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                  child: Image.network(
-                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcb6ttZeCtn6fQLbkh_H0ElEN8WGZMcpLAbQ&usqp=CAU"),
                 ),
               ),
             ),
@@ -62,39 +71,11 @@ class _LogInPageState extends State<LogInPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    primary: Colors.black,
-                    side: const BorderSide(style: BorderStyle.solid, width: 1),
-                  ),
-                  onPressed: () {},
-                  child: const Padding(
-                    padding: EdgeInsets.all(4.0),
-                    child: Text(
-                      "Log In",
-                      style:
-                          TextStyle(fontWeight: FontWeight.w300, fontSize: 22),
-                    ),
-                  ),
-                ),
+                button(name: "Log In", tittle: 'Logged In', ctx: context),
                 const SizedBox(
                   width: 20,
                 ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    primary: Colors.black,
-                    side: const BorderSide(style: BorderStyle.solid, width: 1),
-                  ),
-                  onPressed: () {},
-                  child: const Padding(
-                    padding: EdgeInsets.all(4.0),
-                    child: Text(
-                      "Sıgn Up",
-                      style:
-                          TextStyle(fontWeight: FontWeight.w300, fontSize: 22),
-                    ),
-                  ),
-                ),
+                button(name: "Sign Up", tittle: 'Sign Up', ctx: context)
               ],
             ),
           ],
@@ -102,4 +83,37 @@ class _LogInPageState extends State<LogInPage> {
       ),
     );
   }
+}
+
+String func(bool toogle) {
+  if (!toogle) {
+    return "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcb6ttZeCtn6fQLbkh_H0ElEN8WGZMcpLAbQ&usqp=CAU";
+  }
+  return "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Da_Vinci_Vitruve_Luc_Viatour.jpg/360px-Da_Vinci_Vitruve_Luc_Viatour.jpg";
+}
+
+Widget button(
+    {required String name, required BuildContext ctx, required String tittle}) {
+  return OutlinedButton(
+    style: OutlinedButton.styleFrom(
+      primary: Colors.black,
+      side: const BorderSide(style: BorderStyle.solid, width: 1),
+    ),
+    onPressed: () {
+      Navigator.push(
+        ctx,
+        MaterialPageRoute(
+            builder: (context) => MyHomePage(
+                  title: tittle,
+                )),
+      );
+    },
+    child: Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Text(
+        name,
+        style: const TextStyle(fontWeight: FontWeight.w300, fontSize: 22),
+      ),
+    ),
+  );
 }
