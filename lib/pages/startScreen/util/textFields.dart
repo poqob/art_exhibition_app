98a8fbe2b76e2b689603a92e0b8a5ce6a6298720
Mustafa <mmermmus123@gmail.com>
2022-11-01 19:49:13 +0300

    big update, code cleaned.

--- lib/pages/startScreen/util/textFields.dart
+++ lib/pages/startScreen/util/textFields.dart
@@ -1,3 +1,6 @@
+// ignore: file_names
+import 'package:art_exhibition_app/utils/Todo.dart';
+import 'package:art_exhibition_app/utils/db_methods.dart';
 import 'package:flutter/material.dart';
 
 class TextInput extends StatefulWidget {
@@ -6,21 +9,27 @@ class TextInput extends StatefulWidget {
     this.hintTextt = "",
     this.suffixTextt = "",
     this.isObsecure = false,
+    required this.textInputType,
+    required this.formKey,
   }) : super(
           key: key,
         );
+  final GlobalKey formKey;
   final String hintTextt;
   final String suffixTextt;
   final bool isObsecure;
-
+  final TextInputType textInputType;
   @override
   State<TextInput> createState() => _TextInputState();
 }
 
 class _TextInputState extends State<TextInput> {
+  DbMethods db = DbMethods();
+
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      keyboardType: widget.textInputType,
       obscureText: widget.isObsecure,
       maxLength: 10,
       cursorWidth: 1.5 * 2,
@@ -47,6 +56,26 @@ class _TextInputState extends State<TextInput> {
         suffixStyle: const TextStyle(color: Colors.white70),
         fillColor: Colors.white,
       ),
+      //@TODO: database query will comes up hereee !!!
+      validator: (value) {
+        @Todo('how can i pass future object to validator?');
+        print(db.loginAuth(value!));
+
+        if (value == null || value.isEmpty) {
+          return 'Please enter some text';
+        }
+
+        if (value == db.loginAuth(value)) {
+          Future<void>.delayed(
+            const Duration(seconds: 1),
+            () => Navigator.popAndPushNamed(context, 'home'),
+          );
+        } else {
+          return "kullanıcı adı ya da şifre yanlış";
+        }
+
+        return null;
+      },
     );
   }
 }
