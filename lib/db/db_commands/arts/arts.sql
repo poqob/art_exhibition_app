94a750ce9fe8265e3519ae52f5aa9c3acba0d306
Mustafa <mmermmus123@gmail.com>
2022-12-22 14:31:46 +0300

    fresh start

--- lib/db/db_commands/arts/arts.sql
+++
@@ -1,6 +0,0 @@
-create schema arts;
-
-create table "arts"."arts" (
-   art_id serial PRIMARY KEY UNIQUE,
-   art_name VARCHAR (50) NOT NULL
-);
--- lib/ui/utils/routes.dart
+++ lib/ui/utils/routes.dart
@@ -1,7 +1,6 @@
 import 'package:art_exhibition_app/ui/pages/auth/logIn/log_in.dart';
 import 'package:art_exhibition_app/ui/pages/auth/signUp/sign_up.dart';
 import 'package:art_exhibition_app/ui/pages/homePage/homeScreen.dart';
-import 'package:art_exhibition_app/ui/pages/savedContents/savedContents.dart';
 import 'package:art_exhibition_app/ui/pages/searchContent/search_content_page.dart';
 import 'package:art_exhibition_app/ui/pages/settings/settingsPage/settings.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +11,5 @@ Map<String, Widget Function(BuildContext)> routes = {
   'home': (context) => const FlowPage(title: 'Art Exhibition'),
   'bbr0': (context) => const FlowPage(title: 'Art Exhibition'),
   'bbr1': (context) => const SearchContentPage(),
-  'bbr2': (context) => const SavedContentsPage(),
   'bbr3': (context) => const SettingsPage(),
 };
